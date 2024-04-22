e49fe7f34bd7be9d524b1acfb1cc3089b025c13b
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-22 17:11:57 +0200

    Creata navbar con li e img. Stilizzata li. Backgroun img e padding in style.

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,8 +1,49 @@
 <template>
+    <div class="main-header">
+        <div class="header-image">
+            <img v-for="(image, index) in headerImages" :key="index" :src="image" :alt="'Immagine ' + (index + 1)">
+        </div>
+
+        <div class="navLinks">
+            <nav>
+                <ul>
+                    <li v-for="(link, index) in navLinks" :key="index">
+                        <a :href="link.url">{{ link.text }}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </template>
 
 <script>
+export default {
+    props: {
+        navLinks: {
+            type: Array,
+            default: () => [],
+        },
+        headerImages: {
+            type: Array,
+            default: () => [],
+        },
+    },
+};
 </script>
 
 <style lang="scss" scoped>
+.main-header {
+    background-color: rgba(0, 0, 0, 0.7);
+    padding: 10px 20px;
+}
+
+ul {
+    display: flex;
+    justify-content: space-around;
+}
+
+a {
+    color: white;
+    text-decoration: none;
+}
 </style>
