be21a21d7f94f9c5b7526b8d8500cada9d943ec5
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-22 16:03:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <pageheader />
+  <mainpage />
+  <pagefooter />
+</template>
+
+<script>
+import PageHeader from './components/PageHeader.vue';
+import MainPage from './components/MainPage.vue';
+import PageFooter from './components/PageFooter.vue';
+
+export default {
+  components: {
+    PageHeader,
+    MainPage,
+    PageFooter,
+  },
+}
+</script>
+
+<style lang="scss">
+@use './style/general';
+</style>
