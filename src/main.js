13a54ca44084a38118cf32a6e194c797ef9268b6
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-23 09:46:49 +0200

    Ripristinato main.js.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-
-// Qui inizia il lavoro di importazione da FontAwesome.
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-
-const app = createApp(App)
-
-app.component('font-awesome-icon', FontAwesomeIcon)
-app.mount('#app')
+createApp(App).mount('#app')
