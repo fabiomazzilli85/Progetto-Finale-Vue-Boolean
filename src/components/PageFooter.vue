9ab0749c3b34bdf20e6c217bf300c62b67ba49df
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-23 10:51:51 +0200

    Block1 2 3 4 passati come props. Object.

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,38 +1,69 @@
 <template>
     <footer class="container">
-      <div class="row">
-        <div class="col-md-2">
-          <!-- Contenuto del blocco 1 -->
-          <h2>{{ block1 }}</h2>
+        <div class="row">
+            <div class="col-md-2">
+                <!-- Contenuto del blocco 1 -->
+                <img :src="block1.imageSrc" />
+                <ul>
+                    <li v-for="item in block1.listItems" :key="item.text">
+                        {{ item.text }}
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-md-6">
+                <!-- Contenuto del blocco 2 -->
+
+                <h2>{{ block2.title }}</h2>
+                <p>{{ block2.paragraph }}</p>
+                <div>
+                    <button v-for="(button, index) in block2.buttons" :key="index">
+                        {{ button.text }}
+                    </button>
+                </div>
+            </div>
+
+
+            <div class="col-md-2">
+                <!-- Contenuto del blocco 3 -->
+                <div>
+                    <h2>{{ block3.title }}</h2>
+                    <ul>
+                        <li v-for="item in block3.listItems" :key="item.text">
+                            {{ item.text }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <div class="col-md-2">
+                <!-- Contenuto del blocco 4 -->
+                <div>
+                    <h2>{{ block4.title }}</h2>
+                    <ul>
+                        <li v-for="item in block4.listItems" :key="item.text">
+                            {{ item.text }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="col-md-6">
-          <!-- Contenuto del blocco 2 -->
-          <p>{{ block2 }}</p>
-        </div>
-        <div class="col-md-2">
-          <!-- Contenuto del blocco 3 -->
-          <p>{{ block3 }}</p>
-        </div>
-        <div class="col-md-2">
-          <!-- Contenuto del blocco 4 -->
-          <p>{{ block4 }}</p>
-        </div>
-      </div>
     </footer>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     name: 'PageFooter',
     props: {
-      block1: String,
-      block2: String,
-      block3: String,
-      block4: String,
+        block1: Object,
+        block2: Object,
+        block3: Object,
+        block4: Object,
     },
-  };
-  </script>
-  
+};
+</script>
+
 <style lang="scss" scoped>
 footer {
     background-color: black;
