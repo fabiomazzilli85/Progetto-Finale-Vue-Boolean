825c2f658949e30b0e204d599fc779828d044bbc
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-23 10:14:28 +0200

    Importati i vari block. lavorato con props.

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,8 +1,40 @@
 <template>
-</template>
-
-<script>
-</script>
-
+    <footer class="container">
+      <div class="row">
+        <div class="col-md-2">
+          <!-- Contenuto del blocco 1 -->
+          <h2>{{ block1 }}</h2>
+        </div>
+        <div class="col-md-6">
+          <!-- Contenuto del blocco 2 -->
+          <p>{{ block2 }}</p>
+        </div>
+        <div class="col-md-2">
+          <!-- Contenuto del blocco 3 -->
+          <p>{{ block3 }}</p>
+        </div>
+        <div class="col-md-2">
+          <!-- Contenuto del blocco 4 -->
+          <p>{{ block4 }}</p>
+        </div>
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PageFooter',
+    props: {
+      block1: String,
+      block2: String,
+      block3: String,
+      block4: String,
+    },
+  };
+  </script>
+  
 <style lang="scss" scoped>
+footer {
+    background-color: black;
+}
 </style>
