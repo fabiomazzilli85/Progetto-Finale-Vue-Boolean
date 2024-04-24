1bd04f105a6e297848d327f2920b402544674a53
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-24 11:33:42 +0200

    Rimossa classe color-text.

--- src/components/RecentNews.vue
+++ src/components/RecentNews.vue
@@ -1,45 +1,51 @@
 <template>
-    <section class="bg-white pd-bottom-section">
-        <div class="article">
-            <img class="like" src="../assets/like.png" alt="">
-            <h2 class="color-text">Recent News & Articles</h2>
-            <p class="color-text">Be aware of the football world.</p>
-        </div>
-
-        <div class="flex-article">
-            <div class="card-article">
-                <img src="../assets/news1-1-223x223.png" alt="">
-                <p class="color-text">29.05.2022</p>
-                <h2 class="color-text">What Is Football?</h2>
-                <p class="color-text text-width">Football is a sport that is know as a ball game and loved by[...]</p>
-                <button class="more-button">More</button>
+    <div class="container">
+        <section class="bg-white pd-bottom-section">
+            <div class="article">
+                <img class="like" src="../assets/like.png" alt="">
+                <h2 class="color-text">Recent News & Articles</h2>
+                <p class="color-text">Be aware of the football world.</p>
             </div>
 
-            <div class="card-article">
-                <img src="../assets/news3-1-223x223.png" alt="">
-                <p class="color-text">29.05.2022</p>
-                <h2 class="color-text">Footbal Rules</h2>
-                <p class="color-text text-width">GOLDEN GOAL: in a football match, the goal scored by one of the teamin [...]</p>
-                <button class="more-button">More</button>
-            </div>
+            <div class="flex-article">
+                <div class="card-article">
+                    <img src="../assets/news1-1-223x223.png" alt="">
+                    <p class="color-text">29.05.2022</p>
+                    <h2 class="color-text">What Is Football?</h2>
+                    <p class="color-text text-width">Football is a sport that is know as a ball game and loved by[...]
+                    </p>
+                    <button class="more-button">More</button>
+                </div>
 
-            <div class="card-article">
-                <img src="../assets/news2-1-223x223.png" alt="">
-                <p class="color-text">29.05.2022</p>
-                <h2 class="color-text">Football Features</h2>
-                <p class="color-text text-width">Football; it is an aerobic-based anareobic sport in which jumps, kicks, turns [...]</p>
-                <button class="more-button">More</button>
-            </div>
+                <div class="card-article">
+                    <img src="../assets/news3-1-223x223.png" alt="">
+                    <p class="color-text">29.05.2022</p>
+                    <h2 class="color-text">Footbal Rules</h2>
+                    <p class="color-text text-width">GOLDEN GOAL: in a football match, the goal scored by one of the
+                        teamin [...]</p>
+                    <button class="more-button">More</button>
+                </div>
 
-            <div class="card-article">
-                <img src="../assets/news4-1-223x223.png" alt="">
-                <p class="color-text">29.05.2022</p>
-                <h2 class="color-text">Football Terms</h2>
-                <p class="color-text text-width">Golden Goal: in a football match, the goal scored by one of the teamin[...]</p>
-                <button class="more-button">More</button>
+                <div class="card-article">
+                    <img src="../assets/news2-1-223x223.png" alt="">
+                    <p class="color-text">29.05.2022</p>
+                    <h2 class="color-text">Football Features</h2>
+                    <p class="color-text text-width">Football; it is an aerobic-based anareobic sport in which jumps,
+                        kicks, turns [...]</p>
+                    <button class="more-button">More</button>
+                </div>
+
+                <div class="card-article">
+                    <img src="../assets/news4-1-223x223.png" alt="">
+                    <p class="color-text">29.05.2022</p>
+                    <h2 class="color-text">Football Terms</h2>
+                    <p class="color-text text-width">Golden Goal: in a football match, the goal scored by one of the
+                        teamin[...]</p>
+                    <button class="more-button">More</button>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </template>
 
 <script>
@@ -55,9 +61,6 @@
     width: 100px;
 }
 
-.color-text {
-    color: black;
-}
 
 .text-width {
     width: 220px;
@@ -67,7 +70,7 @@
     background-color: black;
     color: white;
     padding: 15px 35px;
-    border-radius:25px;
+    border-radius: 25px;
     margin: 15px 0;
 }
 
