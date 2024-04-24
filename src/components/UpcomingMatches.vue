315fab23cd813d931f6ef151ffb2c7414909cace
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-24 17:14:44 +0200

    Inseriti nomi e loghi squadre. Impostato bacgroun img e background color RGBA come in esercizio.

---
+++ src/components/UpcomingMatches.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="container">
+        <h2>Upcoming Matches</h2>
+        
+
+        <div class="flex-match">
+            <div class="flex-club">
+                Shalke Club
+                <img class="logo" src="../assets/league-club-3.png">
+            </div>
+            <div class="flex-club">
+                Inter Club
+                <img class="logo" src="../assets/league-club-4.png">
+            </div>
+        </div>
+        <div class="ticket">
+            <p>October 20, 2022, 17.00 PM</p>
+            <p>Espert Stadium</p>
+        </div>
+
+
+        <div class="flex-match">
+            <div class="flex-club">
+                Atletico Club
+                <img class="logo" src="../assets/club-9.png">
+            </div>
+            <div class="flex-club">
+                Shalke Club
+                <img class="logo" src="../assets/club-6.png">
+            </div>
+        </div>
+        <div class="ticket">
+            <p>October 20, 2022, 17.00 PM</p>
+            <p>Espert Stadium</p>
+        </div>
+
+        <div class="flex-match">
+            <div class="flex-club">
+                Unied Fs Club
+                <img class="logo" src="../assets/club-2.png">
+            </div>
+            <div class="flex-club">
+                Bayern Club
+                <img class="logo" src="../assets/club-11.png">
+            </div>
+        </div>
+        <div class="ticket">
+            <p>October 20, 2022, 17.00 PM</p>
+            <p>Espert Stadium</p>
+         </div>
+
+        <div class="flex-match">
+            <div class="flex-club">
+                Arsenal Club
+                <img class="logo" src="../assets/club-7.png">
+            </div>
+             <div class="flex-club">
+                Atletico Club
+                <img class="logo" src="../assets/club-9.png">
+            </div>
+        </div>
+        <div class="ticket">
+             <p>October 20, 2022, 17.00 PM</p>
+            <p>Espert Stadium</p>
+        </div>
+
+
+        <div class="flex-match">
+             <div class="flex-club">
+                 Istanbul Sportds
+                 <img class="logo" src="../assets/club-11.png">
+            </div>
+            <div class="flex-club">
+                United Fs Club
+                <img class="logo" src="../assets/club-2.png">
+            </div>
+        </div>
+        <div class="ticket">
+            <p>October 20, 2022, 17.00 PM</p>
+            <p>Espert Stadium</p>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+.container {
+    background-image: url(../assets/saha.jpg);
+    width: 100%;
+    height: 750px;
+    padding: 20px;
+}
+
+h2 {
+    margin: 15px 0;
+    text-align: center;
+    color: white;
+}
+
+p {
+    color: white
+}
+
+.flex-match {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+    margin: 20px 0;
+    background-color: rgba(200, 200, 255, 0.3);
+}
+
+.flex-club {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    color: white;
+}
+
+.logo {
+    width: 65px;
+    aspect-ratio: 1;
+}
+
+.ticket {
+    display: flex;
+    justify-content: center;
+    gap: 25px;
+    background-color: rgba(50, 50, 150, 0.4);
+}
+</style>
